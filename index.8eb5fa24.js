2d7c4e4ecc2de4132819792c3a49dc48f479abde
tarik2454 <tarik2454@gmail.com>
2022-10-28 12:01:11 +0000

    Deploying to gh-pages from @ tarik2454/IceCream@0861ce2f0f5476e0a73a238b8de40708ac3640fc 🚀

---
+++ index.8eb5fa24.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper1",{autoplay:!0,slidesPerView:"auto",loop:!0,loopedSlides:3,spaceBetween:500,centeredSlides:!0}),new Swiper(".mySwiper2",{autoplay:!0,slidesPerView:"auto",loop:!0,loopedSlides:3,spaceBetween:500,centeredSlides:!0,pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.8eb5fa24.js.map
